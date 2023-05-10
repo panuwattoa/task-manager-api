eb926d96b86daf4a169deee3511d08f7a03c0d6e
Panuwat <panuwat_jarujareet@hotmail.com>
2023-05-10 22:22:22 +0700

    fix: interface task

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -4,21 +4,22 @@ import (
 	"context"
 	"task-manager-api/internal/comment"
 	"task-manager-api/internal/profile"
+	"task-manager-api/internal/taskmanager"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 //go:generate mockgen -source=./handler.go -destination=./mock/handler_mock.go
 type ITasks interface {
-	CreateTask(ctx context.Context, topic string, desc string) error
+	CreateTask(ctx context.Context, topic string, desc string, ownerId string) (*taskmanager.TaskDoc, error)
 }
 type IComments interface {
-	CreateComment(ctx context.Context, ownerID string, topicID string, content string) error
+	CreateComment(ctx context.Context, ownerId string, topicID string, content string) error
 	GetComment(ctx context.Context, id string) (*comment.CommentDoc, error)
 }
 
 type IProfile interface {
-	GetProfile(ctx context.Context, id string) (*profile.ProfileDoc, error)
+	GetProfile(ctx context.Context, ownerId string) (*profile.ProfileDoc, error)
 }
 type Handler struct {
 	task    ITasks
