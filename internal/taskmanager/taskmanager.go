520258a5257ae5cb2ad3274a5501a46a0f685f62
Panuwat <panuwat_jarujareet@hotmail.com>
2023-05-13 17:41:38 +0700

    feat: add logic create task , get, update, ArchiveTask

--- internal/taskmanager/taskmanager.go
+++ internal/taskmanager/taskmanager.go
@@ -36,30 +36,134 @@ const (
 )
 
 type TaskDoc struct {
-	ID          string `bson:"_id,omitempty"`
-	Topic       string `bson:"topic"`
-	Description string `bson:"description"`
-	Status      int    `bson:"status"`
-	CreateDate  int64  `bson:"create_date"`
-	OwnerID     string `bson:"owner_id"`
-	ArchiveDate *int64 `bson:"archive_date"`
-	UpdateDate  *int64 `bson:"update_date"`
+	ID          string `json:"id" bson:"_id,omitempty"`
+	Topic       string `json:"topic" bson:"topic"`
+	Description string `json:"description" bson:"description"`
+	Status      int    `json:"status" bson:"status"`
+	CreateDate  int64  `json:"create_date" bson:"create_date"`
+	OwnerID     string `json:"owner_id" bson:"owner_id"`
+	ArchiveDate *int64 `json:"archive_date" bson:"archive_date"`
+	UpdateDate  *int64 `json:"update_date" bson:"update_date"`
 }
 
-func (t *TaskManager) CreateTask(ctx context.Context, topic string, desc string, ownerId string) (*TaskDoc, error) {
-	return nil, nil
+func (t *TaskManager) CreateTask(ctx context.Context, ownerId string, topic string, desc string) (*TaskDoc, error) {
+	// create new task
+	// TODO: some other business logic here
+	now := t.now().Unix()
+	result, err := t.mongo.InsertOne(ctx, TaskDoc{
+		Topic:       topic,
+		Description: desc,
+		Status:      TaskStatusOpen,
+		CreateDate:  now,
+		OwnerID:     ownerId,
+	})
+	if err != nil {
+		// TODO: log error
+		return nil, err
+	}
+
+	if oid, ok := result.InsertedID.(primitive.ObjectID); ok {
+		return &TaskDoc{
+			ID:          oid.Hex(),
+			Topic:       topic,
+			Description: desc,
+			Status:      TaskStatusOpen,
+			CreateDate:  now,
+			OwnerID:     ownerId,
+		}, nil
+	} else {
+		// TODO: log error
+		return nil, errors.New("cannot convert inserted id to object id")
+	}
+}
+
+func (t *TaskManager) GetAllTask(ctx context.Context, page int, limit int) ([]TaskDoc, error) {
+	// find all task with pagination
+	curr, err := t.mongo.Find(ctx, bson.M{
+		"$or": []bson.M{
+			{
+				"archive_date": bson.M{
+					"$exists": false,
+				},
+			},
+			{
+				"archive_date": nil,
+			},
+		},
+	}, m.NewMongoPaginate(limit, page).GetPaginatedOpts())
+
+	if err != nil {
+		// TODO: log error
+		return nil, err
+	}
+	var tasks = make([]TaskDoc, 0)
+	if err := curr.All(ctx, &tasks); err != nil {
+		return nil, err
+	}
+	return tasks, nil
 }
 
 func (t *TaskManager) GetTask(ctx context.Context, id string) (*TaskDoc, error) {
-	return nil, nil
+	// find task by id
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	curr := t.mongo.FindOne(ctx, bson.M{
+		"_id": objectId,
+		"$or": []bson.M{
+			{
+				"archive_date": bson.M{
+					"$exists": false,
+				},
+			},
+			{
+				"archive_date": nil,
+			},
+		},
+	}, &options.FindOneOptions{
+		Sort: bson.M{
+			"_id": 1,
+		},
+	})
+	var task TaskDoc
+	if err := curr.Decode(&task); err != nil {
+		return nil, err
+	}
+	return &task, nil
 }
 
-func (t *TaskManager) UpdateTaskStatus(ctx context.Context, id string, status int) error {
+func (t *TaskManager) UpdateTaskStatus(ctx context.Context, ownerId string, id string, status int) error {
+	// update task status
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	_, err := t.mongo.UpdateOne(ctx, bson.M{
+		"_id":      objectId,
+		"owner_id": ownerId,
+	}, bson.M{
+		"$set": bson.M{
+			"status":      status,
+			"update_date": t.now().Unix(),
+		},
+	})
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-func (t *TaskManager) ArchiveTask(ctx context.Context, id string) error {
-	return nil
+func (t *TaskManager) ArchiveTask(ctx context.Context, ownerId string, id string) (int, error) {
+	// archive task
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	results, err := t.mongo.UpdateOne(ctx, bson.M{
+		"_id":      objectId,
+		"owner_id": ownerId,
+	}, bson.M{
+		"$set": bson.M{
+			"archive_date": t.now().Unix(),
+			"update_date":  t.now().Unix(),
+		},
+	})
+	if err != nil {
+		return 0, err
+	}
+	return int(results.MatchedCount), nil
 }
 
 func (t *TaskManager) now() time.Time {
