e527a74cc6d633ef059ac73513a9f876a363246b
Panuwat <panuwat_jarujareet@hotmail.com>
2023-05-13 17:41:56 +0700

    feat: add profile service logic get

--- internal/profile/profile.go
+++ internal/profile/profile.go
@@ -2,21 +2,27 @@ package profile
 
 import (
 	"context"
+	"errors"
 	iMongo "task-manager-api/internal/mongo"
+	m "task-manager-api/internal/mongo"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-//go:generate mockgen -source=./profile.go -destination=./mock/profile_mock.go
+//go:generate mockgen -source=./profile.go -destination=./mock/profile.go
 type IMongo interface {
 	FindOne(ctx context.Context, filter interface{}, opts ...*options.FindOneOptions) iMongo.SingleResult
+	Find(ctx context.Context, filter interface{}, opts ...*options.FindOptions) (m.Cursor, error)
 }
 
 type ProfileDoc struct {
 	OwnerID     string `json:"owner_id" bson:"owner_id"`
 	DisplayName string `json:"display_name" bson:"display_name"`
 	Email       string `json:"email" bson:"email"`
+	DisplayPic  string `json:"display_pic" bson:"display_pic"`
 	UpdateDate  int64  `bson:"update_date"`
 	CreateDate  int64  `bson:"create_date"`
 }
@@ -31,7 +37,29 @@ func NewProfileService(mongo IMongo) *Profile {
 }
 
 func (p *Profile) GetProfile(ctx context.Context, ownerId string) (*ProfileDoc, error) {
-	return nil, nil
+	result := p.mongo.FindOne(ctx, bson.M{"owner_id": ownerId})
+	profile := new(ProfileDoc)
+	if err := result.Decode(profile); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return profile, nil
+}
+
+func (p *Profile) GetProfileList(ctx context.Context, ownerId []string) ([]ProfileDoc, error) {
+	curr, err := p.mongo.Find(ctx, bson.M{"owner_id": bson.M{"$in": ownerId}})
+
+	if err != nil {
+		// TODO: log error
+		return nil, err
+	}
+	var profiles = make([]ProfileDoc, 0)
+	if err := curr.All(ctx, &profiles); err != nil {
+		return nil, err
+	}
+	return profiles, nil
 }
 
 func (p *Profile) now() time.Time {
